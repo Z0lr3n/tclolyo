081c421df2987b2ebc50a3c4504c928950b3ce6c
Z0lr3n <vincent.perrault@epitech.eu>
2024-02-15 15:05:44 +0100

    routes

--- routes/api.php
+++ routes/api.php
@@ -16,5 +16,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 
-Route::get('/counter/add', [CounterController::class, 'add']);
+Route::post('/counter/add', [CounterController::class, 'add']);
 Route::get('/counter/count', [CounterController::class, 'get']);
